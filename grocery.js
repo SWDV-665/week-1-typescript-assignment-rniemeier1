9f082cbc60aeb218e45036c9292bc9ec28bd0593
R. Niemeier <48067061+rniemeier@users.noreply.github.com>
2022-10-23 22:14:05 -0500

    Finishing Touches

--- grocery.js
+++ grocery.js
@@ -1,5 +1,6 @@
 /*
 Rebecca Niemeier
+10/23/2022
 Create Grocery.ts, define the Grocery class, add Grocery items to an arrary, then display the items with HTML output.
 */
 var _a;
@@ -17,16 +18,18 @@ var Grocery = /** @class */ (function () {
     };
     return Grocery;
 }());
-//add grocery items to array
+//create Grocery array and add items
 var groceryList = [];
 groceryList.push(new Grocery('milk', 3, 3893, 'A23'));
 groceryList.push(new Grocery('lettuce', 6, 1023, 'B3'));
 groceryList.push(new Grocery('beans', 11, 9399, 'L3'));
-if (document.getElementById("output") == null) { //if output div doens't exist, make it exist
+//Verify output div exists, and if it doesn't, then create it and add it to the body
+if (document.getElementById("output") == null) {
     var temp = document.createElement("div");
     temp.setAttribute("id", "output");
     document.body.appendChild(temp);
 }
+//Get the output div, then add each Grocery item from the array (and add a </br> element between them for formatting)
 var outputDiv = (_a = document.getElementById("output")) !== null && _a !== void 0 ? _a : document.createElement("div");
 for (var i = 0; i < groceryList.length; i++) {
     var newChild = document.createTextNode(groceryList[i].makeString());
--- grocery.ts
+++ grocery.ts
@@ -1,5 +1,6 @@
 /*
 Rebecca Niemeier
+10/23/2022
 Create Grocery.ts, define the Grocery class, add Grocery items to an arrary, then display the items with HTML output. 
 */
 
@@ -24,18 +25,20 @@ class Grocery {
 }
 
 
-//add grocery items to array
+//create Grocery array and add items
 const groceryList: Grocery[] = [];
 groceryList.push(new Grocery('milk', 3, 3893, 'A23'));
 groceryList.push(new Grocery('lettuce', 6, 1023, 'B3'));
 groceryList.push(new Grocery('beans', 11, 9399, 'L3'));
 
-if(document.getElementById("output") == null){ //if output div doens't exist, make it exist
+//Verify output div exists, and if it doesn't, then create it and add it to the body
+if(document.getElementById("output") == null){ 
     var temp  = document.createElement("div");
     temp.setAttribute("id", "output");
     document.body.appendChild(temp);
 }
 
+//Get the output div, then add each Grocery item from the array (and add a </br> element between them for formatting)
 var outputDiv = document.getElementById("output") ?? document.createElement("div");
 for(let i = 0; i < groceryList.length; i++){
     const newChild = document.createTextNode(groceryList[i].makeString());
